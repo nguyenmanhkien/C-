1108ad025debe948cf481f852954f906a2db5c11
nguyenmanhkien <nguyenmanhkien290103@gmail.com>
2022-06-21 09:32:56 +0700

    SS1

---
+++ Session01/AnonTypes/AnonTypes.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+class AnonTypes
+{
+    static void Main(string[] args)
+    {
+        var pl = new { Name = "A", Price = 3 };
+        Console.WriteLine("Name = {0} \nPrice = {1}", pl.Name.ToLower(), pl.Price);
+        Console.ReadLine();
+    }
+}
---
+++ Session01/ImplicitilyTypedLocal/ImplicitilyTypedLocal.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+class implicitilyTypedLocal
+{
+    static void Main(string[] args)
+    {
+        var i = 5;
+        var s = "hello";
+        var d = 1.0;
+        Console.WriteLine("i*i: "+i*i);
+        Console.WriteLine("s in upper case: " + s.ToUpper());
+        Console.WriteLine("type of d: " + d.GetType());
+        Console.ReadLine();
+    }
+}
